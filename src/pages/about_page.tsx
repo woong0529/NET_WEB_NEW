2558ac5307d5ef11c12a0df82d3240c00cd3c9c7
고강민 <plusts22@gmail.com>
2026-04-14 22:04:57 +0900

    고강민 사이트 기여도 추가

--- src/pages/about_page.tsx
+++ src/pages/about_page.tsx
@@ -43,7 +43,7 @@ const executives = [
     id: 6,
     name: '고강민',
     position: '기획부장',
-    message: '한마디',
+    message: '사이트 기여도 추가',
     image: 'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?w=400&h=400&fit=crop'
   }
 ];
